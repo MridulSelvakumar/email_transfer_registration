5dd37432732054b24895ebb31c1182c74de0d1bb
MridulSelvakumar <mridul07adih@gmail.com>
2024-02-29 11:46:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from app.form import *
+
+from django.http import HttpResponse
+
+def email_transfer(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            send_mail('regsistration',
+            ' Thank U ur Registration is scuccessfull',
+            'mridul07adih@gmail.com',
+            [MUFDO.email],
+            fail_silently=False,
+            )
+            return HttpResponse('Registartion Is Successfull')
+        else:
+            return HttpResponse('Invalid Data')
+
+    return render(request,'register.html',d)
